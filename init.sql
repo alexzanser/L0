6a0e5091f5bcfa8d3bd1e18d10340ef690d48e12
Kortney Mercy <kmercy@et-j1.kzn.21-school.ru>
2022-04-07 12:54:43 +0300

    1

--- init.sql
+++ init.sql
@@ -1,5 +1,45 @@
-CREATE TABLE IF NOT EXISTS ORDERS (
+CREATE TABLE IF NOT EXISTS orders (
     order_uid varchar PRIMARY KEY,
     track_number varchar,
     entry varchar
+);
+
+CREATE TABLE IF NOT EXISTS delivery (
+    order_uid   references orders,
+    name        varchar,
+    phone       varchar,
+    zip         varchar,
+    city        varchar,
+    address     varchar,
+    region      varchar,
+    email       varchar
+);
+
+CREATE TABLE IF NOT EXISTS payment (
+    order_uid       references orders,
+    transaction     varchar,
+    request_id      varchar,
+    currency        varchar,
+    provider        varchar,
+    amount          integer CHECK(VALUE > 0),
+    payment_dt      integer CHECK(VALUE > 0),
+    bank            varchar,
+    delivery_cost   integer CHECK(VALUE >= 0),
+    good_total      integer CHECK(VALUE > 0),
+    custom_fee      integer CHECK(VALUE >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    order_uid       references orders,
+    chrt_id         integer NOT NULL,
+    track_number    references orders(track_number),
+    price           integer CHECK(VALUE > 0),
+    rid             varchar,
+    name            varchar,
+    sale            integer CHECK(VALUE >= 0),
+    size            varchar,
+    total_price     integer CHECK(VALUE > 0),
+    nm_id           integer CHECK(VALUE >= 0),
+    brand           varchar,
+    status          integer
 );
