9af30f43208498031da17bdca07f528871e4bdad
Kortney Mercy <kmercy@et-j1.kzn.21-school.ru>
2022-04-07 13:01:50 +0300

    1

--- init.sql
+++ init.sql
@@ -21,25 +21,25 @@ CREATE TABLE IF NOT EXISTS payment (
     request_id      varchar,
     currency        varchar,
     provider        varchar,
-    amount          integer CHECK(VALUE > 0),
-    payment_dt      integer CHECK(VALUE > 0),
+    amount          integer CHECK(amount > 0),
+    payment_dt      integer CHECK(payment_dt > 0),
     bank            varchar,
-    delivery_cost   integer CHECK(VALUE >= 0),
-    good_total      integer CHECK(VALUE > 0),
-    custom_fee      integer CHECK(VALUE >= 0)
+    delivery_cost   integer CHECK(delivery_cost >= 0),
+    good_total      integer CHECK(good_total > 0),
+    custom_fee      integer CHECK(custom_fee >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS items (
     order_uid       varchar REFERENCES orders,
     chrt_id         integer NOT NULL,
     track_number    varchar REFERENCES orders(track_number),
-    price           integer CHECK(VALUE > 0),
+    price           integer CHECK(price > 0),
     rid             varchar,
     name            varchar,
-    sale            integer CHECK(VALUE >= 0),
+    sale            integer CHECK(sale >= 0),
     size            varchar,
-    total_price     integer CHECK(VALUE > 0),
-    nm_id           integer CHECK(VALUE >= 0),
+    total_price     integer CHECK(total_price > 0),
+    nm_id           integer CHECK(nm_id >= 0),
     brand           varchar,
     status          integer
 );
