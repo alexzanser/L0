fa210c0d889cb98cdc31d2888f03f677d56ae43b
Kortney Mercy <kmercy@et-j1.kzn.21-school.ru>
2022-04-07 14:13:01 +0300

    1

--- internal/domain/store.go
+++ internal/domain/store.go
@@ -1,28 +1,5 @@
-package domain
+package store
 
-type Order struct {
-	OrderUid		string		`json:"order_uid"`
-	TrackNumber		string		`json:"track_number"`
-	Entry			string		`json:"entry"`
-	Delivery		Delivery	`json:"delivery"`
-	Items			[]Item		`json:"items"`	
+type Store struct {
+	
 }
-
-type Delivery struct {
-	Name	string	`json:"name"`
-	Phone	string	`json:"phone"`
-	Zip		string	`json:"zip"`
-	City	string	`json:"city"`
-	Address	string	`json:"address"`
-	Region	string	`json:"region"`
-	Email	string	`json:"email"`
-}
-
-type Item struct {
-	ChrtId			int 	`json:"chrt_id"`
-	TrackNumber		string	`json:"track_number"`
-	Price			int		`json:"price"`
-	Rid				string	`json:"rid"`
-
-}
-
