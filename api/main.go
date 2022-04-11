df97fa2748293c639578f3f1a5c30d7b69f9c1cf
Kortney Mercy <kmercy@et-j3.kzn.21-school.ru>
2022-04-11 16:59:57 +0300

    1

--- api/main.go
+++ api/main.go
@@ -14,7 +14,7 @@ import (
 const (
 	clusterID = "test-cluster"
 	clientID = "client-222"
-	connstr = "postgres://user_go:8956_go@localhost:5422/orders"
+	connstr = "postgres://user_go:8956_go@localhost:5432/orders"
 )
 
 func main() {
